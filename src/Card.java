696fbd1a9487939af255360803b9eeaa364e9a68
Felipe Fernando Baie dos santos <felipefernandobss@gmail.com>
2026-07-07 21:01:38 -0300

    feat: add Purchase class and update Card attributes
    - Add Purchase class with value, description and date attributes.
    - Add avaliableLimit attribute to Card.
    - Separate static nextNumber counter from instance number attribute.

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Card {
+    private static int nextNumber = 1000;
+    private double creditLimit = 2000;
+    private double avaliableLimit = 2000;
+    private int number;
+
+
+    private List<Card> shoppingList = new ArrayList<>();
+}
---
+++ src/Purchase.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class Purchase {
+    private double value;
+    private String description;
+    private LocalDate date;
+}
